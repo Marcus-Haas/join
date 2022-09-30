fa54c2683637d8085a34cc480655b4989de158b1
Marcus-Haas <marcus-haas@hotmail.de>
2022-09-30 11:56:06 +0200

    adding first part of legal notice

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,12 @@ function init() {
     includeHTML();
 }
 
+// Include HTML Templates //
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "include sidebar"
+        file = element.getAttribute("w3-include-html"); // "include html"
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
