62f3e6050a04d5ef0d7f2f52c15bdf1b8f6448bd
anilorhan -fwfac <orhan_anil@yahoo.de>
2022-10-08 15:18:14 +0200

    update drag and drop

--- assets/js/board.js
+++ assets/js/board.js
@@ -3,6 +3,9 @@ let description = [];
 let category = [];
 let assigned = [];
 let duedate = [];
+let id = [];
+let statusContainer = [];
+let prior = [];
 let currentTitle;
 let currentDescription;
 let currentCategory;
@@ -10,14 +13,14 @@ let currentAssigned;
 let currentDuedate;
 let currentDraggedElement;
 let index = 0;
-let prior;
+let currentPrior;
 
 
 /*
 * render todo box
 */
 function createTodo() {
-    updateArray();
+    updateArrayTodo();
     let todo = document.getElementById('todo');
     todo.innerHTML += templateCreateTodo();
     currentTitle.value = ``;
@@ -26,33 +29,61 @@ function createTodo() {
     changeColorAfterCreateTask();
     closeForm();
     changeColorOfCategory();
-
-    if (prior == 'Urgent') {
-        let firstImage = document.getElementById('createFirstImg');
-        let secondImage = document.getElementById('createSecondImg');
-        firstImage.src = "assets/img/board/arrow-urgent.svg";
-        secondImage.src = "assets/img/board/arrow-urgent.svg";
-    }
-    if (prior == 'Medium') {
-        let firstImage = document.getElementById('createFirstImg');
-        let secondImage = document.getElementById('createSecondImg');
-        firstImage.src = "assets/img/board/arrow-medium.svg";
-        secondImage.src = "assets/img/board/arrow-medium.svg";
-    }
-    if (prior == 'Low') {
-        let firstImage = document.getElementById('createFirstImg');
-        let secondImage = document.getElementById('createSecondImg');
-        firstImage.src = "assets/img/board/arrow-low.svg";
-        secondImage.src = "assets/img/board/arrow-low.svg";
-    }
+    changePrior();
     index++;
 }
 
 
+function changePrior() {
+    if (currentPrior == 'Urgent') {
+        let firstImage = document.getElementById('createFirstImg' + index);
+        let secondImage = document.getElementById('createSecondImg' + index);
+        firstImage.src = "assets/img/board/arrow-urgent.svg";
+        secondImage.src = "assets/img/board/arrow-urgent.svg";
+        prior.push(currentPrior);
+    }
+    if (currentPrior == 'Medium') {
+        let firstImage = document.getElementById('createFirstImg' + index);
+        let secondImage = document.getElementById('createSecondImg' + index);
+        firstImage.src = "assets/img/board/arrow-medium.svg";
+        secondImage.src = "assets/img/board/arrow-medium.svg";
+        prior.push(currentPrior);
+    }
+    if (currentPrior == 'Low') {
+        let firstImage = document.getElementById('createFirstImg' + index);
+        let secondImage = document.getElementById('createSecondImg' + index);
+        firstImage.src = "assets/img/board/arrow-low.svg";
+        secondImage.src = "assets/img/board/arrow-low.svg";
+        prior.push(currentPrior);
+    }
+}
+
+
+function changePriorAfterDragAndDrop(i) {
+    if (prior[i] == 'Urgent') {
+        let firstImage = document.getElementById('createFirstImg' + i);
+        let secondImage = document.getElementById('createSecondImg' + i);
+        firstImage.src = "assets/img/board/arrow-urgent.svg";
+        secondImage.src = "assets/img/board/arrow-urgent.svg";
+    }
+    if (prior[i] == 'Medium') {
+        let firstImage = document.getElementById('createFirstImg' + i);
+        let secondImage = document.getElementById('createSecondImg' + i);
+        firstImage.src = "assets/img/board/arrow-medium.svg";
+        secondImage.src = "assets/img/board/arrow-medium.svg";
+    }
+    if (prior[i] == 'Low') {
+        let firstImage = document.getElementById('createFirstImg' + i);
+        let secondImage = document.getElementById('createSecondImg' + i);
+        firstImage.src = "assets/img/board/arrow-low.svg";
+        secondImage.src = "assets/img/board/arrow-low.svg";
+    }
+}
+
 /*
 * render todo box
 */
-function updateArray() {
+function updateArrayTodo() {
     currentTitle = document.getElementById('title');
     title.push(currentTitle.value);
     currentDescription = document.getElementById('descriptionPopup');
@@ -63,103 +94,168 @@ function updateArray() {
     assigned.push(currentAssigned.value);
     currentDuedate = document.getElementById('duedate');
     duedate.push(currentDuedate.value);
+    statusContainer.push('todo');
+    id.push(index);
 }
 
 
 function templateCreateTodo() {
     return `    
-    <div draggable="true" id="startDragging${index}" ondrag="dragging()" class="box">
+    <div draggable="true" ondragstart="startDragging(${index})" class="box">
         <div id="changeColorOfCategory${index}" class="category">${category[index]}</div>
         <div class="title">${title[index]}</div>
         <div class="description">${description[index]}</div>
         <div class="assigned-and-prio">
             <div class="assigned">${assigned[index]}</div>
                 <div class="prio">
-                    <div class="first-arrow"><img id="createFirstImg" src=""></div>
-                    <div class="second-arrow"><img id="createSecondImg" src=""></div>
+                    <div class="first-arrow"><img id="createFirstImg${index}" src=""></div>
+                    <div class="second-arrow"><img id="createSecondImg${index}" src=""></div>
                 </div>
         </div>
     </div>`;
 }
 
 
+function updateTodo() {
+    let todo = document.getElementById('todo');
+    todo.innerHTML = ``;
+    for (let i = 0; i < statusContainer.length; i++) {
+        if (statusContainer[i] == 'todo') {
+            todo.innerHTML += templateUpdateTodo(i);
+            changeColorOfCategoryAfterDragAndDrop(i);
+            changePriorAfterDragAndDrop(i);
+        }
+    }
+}
+
+
+function templateUpdateTodo(i) {
+    return `    
+    <div draggable="true" ondragstart="startDragging(${i})" class="box">
+        <div id="changeColorOfCategoryAfterDragAndDrop${i}" class="category">${category[i]}</div>
+        <div class="title">${title[i]}</div>
+        <div class="description">${description[i]}</div>
+        <div class="assigned-and-prio">
+            <div class="assigned">${assigned[i]}</div>
+                <div class="prio">
+                    <div class="first-arrow"><img id="createFirstImg${i}" src=""></div>
+                    <div class="second-arrow"><img id="createSecondImg${i}" src=""></div>
+                </div>
+        </div>
+    </div>`;
+}
+
+
+function updateInProgress() {
+    let inProgress = document.getElementById('inProgress');
+    inProgress.innerHTML = ``;
+    for (let i = 0; i < statusContainer.length; i++) {
+        if (statusContainer[i] == 'inProgress') {
+            inProgress.innerHTML += templateUpdateInProgress(i);
+            changeColorOfCategoryAfterDragAndDrop(i);
+            changePriorAfterDragAndDrop(i);
+        }
+    }
+}
+
+
+function templateUpdateInProgress(i) {
+    return `    
+    <div draggable="true" ondragstart="startDragging(${i})" class="box">
+        <div id="changeColorOfCategoryAfterDragAndDrop${i}" class="category">${category[i]}</div>
+        <div class="title">${title[i]}</div>
+        <div class="description">${description[i]}</div>
+        <div class="assigned-and-prio">
+            <div class="assigned">${assigned[i]}</div>
+                <div class="prio">
+                    <div class="first-arrow"><img id="createFirstImg${i}" src=""></div>
+                    <div class="second-arrow"><img id="createSecondImg${i}" src=""></div>
+                </div>
+        </div>
+    </div>`;
+}
+
+
+
+function updateAwaitingFeedback() {
+    let awaitingFeedback = document.getElementById('awaitingFeedback');
+    awaitingFeedback.innerHTML = ``;
+    for (let i = 0; i < statusContainer.length; i++) {
+        if (statusContainer[i] == 'awaitingFeedback') {
+            awaitingFeedback.innerHTML += templateUpdateAwaitingFeedback(i);
+            changeColorOfCategoryAfterDragAndDrop(i);
+            changePriorAfterDragAndDrop(i);
+        }
+    }
+}
+
+
+function templateUpdateAwaitingFeedback(i) {
+    return `    
+    <div draggable="true" ondragstart="startDragging(${i})" class="box">
+        <div id="changeColorOfCategoryAfterDragAndDrop${i}" class="category">${category[i]}</div>
+        <div class="title">${title[i]}</div>
+        <div class="description">${description[i]}</div>
+        <div class="assigned-and-prio">
+            <div class="assigned">${assigned[i]}</div>
+                <div class="prio">
+                    <div class="first-arrow"><img id="createFirstImg${i}" src=""></div>
+                    <div class="second-arrow"><img id="createSecondImg${i}" src=""></div>
+                </div>
+        </div>
+    </div>`;
+}
+
+
+function updateDone() {
+    let done = document.getElementById('done');
+    done.innerHTML = ``;
+    for (let i = 0; i < statusContainer.length; i++) {
+        if (statusContainer[i] == 'done') {
+            done.innerHTML += templateUpdateDone(i);
+            changeColorOfCategoryAfterDragAndDrop(i);
+            changePriorAfterDragAndDrop(i);
+        }
+    }
+}
+
+
+function templateUpdateDone(i) {
+    return `    
+    <div draggable="true" ondragstart="startDragging(${i})" class="box">
+        <div id="changeColorOfCategoryAfterDragAndDrop${i}" class="category">${category[i]}</div>
+        <div class="title">${title[i]}</div>
+        <div class="description">${description[i]}</div>
+        <div class="assigned-and-prio">
+            <div class="assigned">${assigned[i]}</div>
+                <div class="prio">
+                    <div class="first-arrow"><img id="createFirstImg${i}" src=""></div>
+                    <div class="second-arrow"><img id="createSecondImg${i}" src=""></div>
+                </div>
+        </div>
+    </div>`;
+}
+
 /*
 * drag and drop a task
 */
-function dragging() {
-    const todo = document.querySelector("#startDragging" + (index - 1));
-    todo.addEventListener("dragstart", (ev) => {
-        // Change the source element's background color
-        // to show that drag has started
-        ev.currentTarget.classList.add("dragging");
-        // Clear the drag data cache (for all formats/types)
-        ev.dataTransfer.clearData();
-        // Set the drag's format and data.
-        // Use the event target's id for the data
-        ev.dataTransfer.setData("text/plain", ev.target.id);
-    });
-    todo.addEventListener("dragend", (ev) =>
-        ev.target.classList.remove("dragging")
-    );
-
-
-    if (document.getElementsByClassName('for-height-todo')) {
-        const target = document.querySelector("#todo");
-        target.addEventListener("dragover", (ev) => {
-            ev.preventDefault();
-        });
-        target.addEventListener("drop", (ev) => {
-            ev.preventDefault();
-            // Get the data, which is the id of the source element
-            const data = ev.dataTransfer.getData("text");
-            const todo = document.getElementById(data);
-            ev.target.appendChild(todo);
-        });
-    }
-
-    if (document.getElementsByClassName('for-height-inprogess')) {
-        const target = document.querySelector("#inProgess");
-        target.addEventListener("dragover", (ev) => {
-            ev.preventDefault();
-        });
-        target.addEventListener("drop", (ev) => {
-            ev.preventDefault();
-            // Get the data, which is the id of the source element
-            const data = ev.dataTransfer.getData("text");
-            const todo = document.getElementById(data);
-            ev.target.appendChild(todo);
-        });
-    }
-
-    if (document.getElementsByClassName('for-height-awaitingfeedback')) {
-        const target = document.querySelector("#awaitingFeedback");
-        target.addEventListener("dragover", (ev) => {
-            ev.preventDefault();
-        });
-        target.addEventListener("drop", (ev) => {
-            ev.preventDefault();
-            // Get the data, which is the id of the source element
-            const data = ev.dataTransfer.getData("text");
-            const todo = document.getElementById(data);
-            ev.target.appendChild(todo);
-        });
-    }
-    if (document.getElementsByClassName('for-height-done')) {
-        const target = document.querySelector("#done");
-        target.addEventListener("dragover", (ev) => {
-            ev.preventDefault();
-        });
-        target.addEventListener("drop", (ev) => {
-            ev.preventDefault();
-            // Get the data, which is the id of the source element
-            const data = ev.dataTransfer.getData("text");
-            const todo = document.getElementById(data);
-            ev.target.appendChild(todo);
-        });
-    }
-
+function startDragging(id) {
+    currentDraggedElement = id;
 }
 
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+
+function moveTo(changeStatus) {
+    statusContainer[currentDraggedElement] = changeStatus;
+    updateTodo();
+    updateInProgress();
+    updateAwaitingFeedback();
+    updateDone();
+}
 
 
 /*
@@ -194,7 +290,7 @@ function loadIconAndLine() {
 * change bg color of urgent
 */
 function changeColorUrgent() {
-    prior = document.getElementById('urgentPopup').innerText;
+    currentPrior = document.getElementById('urgentPopup').innerText;
     let urgent = document.getElementById('urgentPopup');
     let medium = document.getElementById('mediumPopup');
     let low = document.getElementById('lowPopup');
@@ -221,7 +317,7 @@ function changeColorUrgent() {
 * change bg color of medium
 */
 function changeColorMedium() {
-    prior = document.getElementById('mediumPopup').innerText;
+    currentPrior = document.getElementById('mediumPopup').innerText;
     let urgent = document.getElementById('urgentPopup');
     let medium = document.getElementById('mediumPopup');
     let low = document.getElementById('lowPopup');
@@ -248,7 +344,7 @@ function changeColorMedium() {
 * change bg color of low
 */
 function changeColorLow() {
-    prior = document.getElementById('lowPopup').innerText;
+    currentPrior = document.getElementById('lowPopup').innerText;
     let urgent = document.getElementById('urgentPopup');
     let medium = document.getElementById('mediumPopup');
     let low = document.getElementById('lowPopup');
@@ -318,9 +414,8 @@ function changeColorAfterCreateTask() {
 
 
 function changeColorOfCategory() {
-    let category = document.getElementById('category-popup')
     let categoryAddColor = document.getElementById('changeColorOfCategory' + index);
-    category = category.value;
+    let category = categoryAddColor.innerText;
     if (category == 'Sales') {
         categoryAddColor.classList.add('sales');
     }
@@ -339,13 +434,11 @@ function changeColorOfCategory() {
     if (category == 'Media') {
         categoryAddColor.classList.add('media');
     }
-
 }
 
-function changeColorOfCategoryAfterDragAndDrop() {
-    let category = document.getElementById('category-popup')
-    let categoryAddColor = document.getElementById('changeColorOfCategoryAfterDragAndDrop' + currentDraggedElement);
-    category = category.value;
+function changeColorOfCategoryAfterDragAndDrop(i) {
+    let categoryAddColor = document.getElementById('changeColorOfCategoryAfterDragAndDrop' + i);
+    let category = categoryAddColor.innerText;
     if (category == 'Sales') {
         categoryAddColor.classList.add('sales');
     }
