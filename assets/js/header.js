8c37437ad94851ff3d11274ec9378744f5f7256c
Marcus-Haas <marcus-haas@hotmail.de>
2022-10-16 12:03:43 +0200

    responsive update

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,8 +2,11 @@ function showLogout() {
     document.getElementById('header-logout').innerHTML = /*html*/ `
     <div class="popup-frame-logout" id="hide-btn" onclick="hideLogout()">
         <div onclick="doNotClose(event)">
-            <div class="logout-btn" onclick="Logout()">
-                <div class="logout-inner-btn">Log out</div>
+            <div class="logout-btn">
+                <div class="rs-logout" onclick="openHelp()">Help</div>
+                <div class="rs-logout" onclick="openLegalNotice()">Legal notice</div>
+                <div class="rs-logout" onclick="openPrivacy()">Privacy</div>
+                <div class="logout-inner-btn" onclick="Logout()">Log out</div>
             </div>
         </div>
     </div>
@@ -24,5 +27,20 @@ function doNotClose(event) {
 async function Logout() {
     await backend.setItem('activeUser', JSON.stringify(activeUser.length = 0));
     window.open("index.html", "_self");
-    
+
+}
+
+
+function openPrivacy() {
+    window.open("privacy.html", "_self");
+}
+
+
+function openHelp() {
+    window.open("help.html", "_self");
+}
+
+
+function openLegalNotice() {
+    window.open("legal-notice.html", "_self");
 }
